a5f4b18823ad755b7473ef15db5b392be84fa696
Daniil Galtsev <galtsev.daniil@gmail.com>
2023-08-23 06:18:16 +0000

    add initial argument parsing

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log("Main.js");
+function main() {
+  const argl = process.argv.length;
+  if (argl < 3) {
+    console.error("Missing argument: url");
+    process.exit(1);
+  }
+  if (argl > 3) {
+    console.error("Too many arguments");
+    process.exit(1);
+  }
+
+  const baseUrl = process.argv[2];
+  console.log(`Starting crawling from '${baseUrl}'...`);
+}
+
+main();
